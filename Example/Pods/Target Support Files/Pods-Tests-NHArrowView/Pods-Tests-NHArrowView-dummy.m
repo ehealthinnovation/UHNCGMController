408077833c03611a659d378d51b86e76ac9a1cf8
Nathaniel Hamming <nhamming@ehealthinnovation.org>
2015-02-23 10:24:27 -0400

    Removing unneeded pods from the pod spec and adding needed pods to the podfile

--- Example/Pods/Target Support Files/Pods-Tests-NHArrowView/Pods-Tests-NHArrowView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tests_NHArrowView : NSObject
-@end
-@implementation PodsDummy_Pods_Tests_NHArrowView
-@end
--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -12,29 +12,29 @@
 #define COCOAPODS_VERSION_MINOR_Expecta 3
 #define COCOAPODS_VERSION_PATCH_Expecta 2
 
-// Expecta+Snapshots
-#define COCOAPODS_POD_AVAILABLE_Expecta_Snapshots
-#define COCOAPODS_VERSION_MAJOR_Expecta_Snapshots 1
-#define COCOAPODS_VERSION_MINOR_Expecta_Snapshots 3
-#define COCOAPODS_VERSION_PATCH_Expecta_Snapshots 1
+// OCHamcrest
+#define COCOAPODS_POD_AVAILABLE_OCHamcrest
+#define COCOAPODS_VERSION_MAJOR_OCHamcrest 4
+#define COCOAPODS_VERSION_MINOR_OCHamcrest 1
+#define COCOAPODS_VERSION_PATCH_OCHamcrest 1
 
-// FBSnapshotTestCase
-#define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
-#define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
-#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 5
-#define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
-
-// NHArrowView
-#define COCOAPODS_POD_AVAILABLE_NHArrowView
-#define COCOAPODS_VERSION_MAJOR_NHArrowView 0
-#define COCOAPODS_VERSION_MINOR_NHArrowView 1
-#define COCOAPODS_VERSION_PATCH_NHArrowView 0
+// OCMockito
+#define COCOAPODS_POD_AVAILABLE_OCMockito
+#define COCOAPODS_VERSION_MAJOR_OCMockito 1
+#define COCOAPODS_VERSION_MINOR_OCMockito 4
+#define COCOAPODS_VERSION_PATCH_OCMockito 0
 
 // Specta
 #define COCOAPODS_POD_AVAILABLE_Specta
 #define COCOAPODS_VERSION_MAJOR_Specta 0
 #define COCOAPODS_VERSION_MINOR_Specta 3
 #define COCOAPODS_VERSION_PATCH_Specta 2
+
+// UHNBLEController
+#define COCOAPODS_POD_AVAILABLE_UHNBLEController
+#define COCOAPODS_VERSION_MAJOR_UHNBLEController 0
+#define COCOAPODS_VERSION_MINOR_UHNBLEController 1
+#define COCOAPODS_VERSION_PATCH_UHNBLEController 0
 
 // UHNCGMController
 #define COCOAPODS_POD_AVAILABLE_UHNCGMController
@@ -47,10 +47,4 @@
 #define COCOAPODS_VERSION_MAJOR_UHNDebug 0
 #define COCOAPODS_VERSION_MINOR_UHNDebug 1
 #define COCOAPODS_VERSION_PATCH_UHNDebug 0
-
-// UHNTimeSeriesPlotView
-#define COCOAPODS_POD_AVAILABLE_UHNTimeSeriesPlotView
-#define COCOAPODS_VERSION_MAJOR_UHNTimeSeriesPlotView 0
-#define COCOAPODS_VERSION_MINOR_UHNTimeSeriesPlotView 1
-#define COCOAPODS_VERSION_PATCH_UHNTimeSeriesPlotView 0
 
